a9cb2e29f9f689e04f5d06a367f8649af04bca3e
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-07-23 14:40:51 -0600

    Agrega CountryController

---
+++ src/main/java/com/carlosarroyoam/countries/ws/client/countries/CountryController.java
@@ -0,0 +1,24 @@
+package com.carlosarroyoam.countries.ws.client.countries;
+
+import com.carlosarroyoam.countries.ws.client.wsdl.GetCountryResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+  private final CountryClient countryClient;
+
+  public CountryController(CountryClient countryClient) {
+    this.countryClient = countryClient;
+  }
+
+  @GetMapping(path = "/{countryName}", produces = "application/json")
+  public ResponseEntity<GetCountryResponse> findById(@PathVariable String countryName) {
+    GetCountryResponse response = countryClient.findByName(countryName);
+    return ResponseEntity.ok(response);
+  }
+}
